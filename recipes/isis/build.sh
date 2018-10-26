3a0b1aca63a9dbd16102ba3d9503c7fe544ae924
chrisryancombs <ryancombsjr@gmail.com>
2018-10-26 16:52:50 -0700

    Add isis build release notes commands

--- recipes/isis/build.sh
+++ recipes/isis/build.sh
@@ -1,5 +1,6 @@
 mkdir build
 cd build
 cmake -GNinja -DJP2KFLAG=ON -Dpybindings=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX -Disis3Data=/usgs/cpkgs/isis3/data -Disis3TestData=/usgs/cpkgs/isis3/testData ../isis
-ninja install
+/usgs/cpkgs/isis3/isis3mgr_scripts/createReleaseNotesXML -d 2018-02-10 -o ../isis/src/docsys/documents/ReleaseNotes/ReleaseNotesList.xml --current-branch=dev ../isis/
 ninja docs
+ninja install
